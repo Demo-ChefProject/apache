1b425caca9102927db2b2dc265d8a986fee254dd
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-02 15:58:36 -0400

    Added script to upload cookbooks

--- node_config/node_bootstrap.sh
+++ node_config/node_bootstrap.sh
@@ -5,8 +5,11 @@ cd ~/home/jenkins/workspace/Chef-apache/
 # Creates a role for tomcat
 sudo knife role from file /home/jenkins/workspace/Chef-apache/roles/apache.rb
 
+# Upload cookbooks into Chef Server
+knife upload cookbooks apache 
+
 # Bootstrap a node to its chef server
-sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/ubuntu/.ssh/agiletrailblazers.pem -N Rigil_Node
+sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/ubuntu/.ssh/agiletrailblazers.pem -N Rigil_Node -r 'role[apache]'
 
 # ssh into the chef node and execute the chef client to run its run list from chef server
 
