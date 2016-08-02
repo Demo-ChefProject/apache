2e16ab0d5ae78cca12e09794f15cb28839b1e573
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-02 14:42:29 -0400

    Added jenkins path the config file

--- node_config/node_bootstrap.sh
+++ node_config/node_bootstrap.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-cd ~/home/jenkins/workspace/Chef-CookbookUpload/.chef/
+cd ~/home/jenkins/workspace/Chef-apache/.chef/
 
 # Creates a role for tomcat
-sudo knife role from file /home/jenkins/workspace/Chef-CookbookUpload/roles/tomcat.rb
+sudo knife role from file /home/jenkins/workspace/Chef-apache/roles/tomcat.rb
 
 # Bootstrap a node to its chef server
-sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/jenkins/workspace/Chef-CookbookUpload/.chef/agiletrailblazers.pem -N Rigil_Nod$
+sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/jenkins/workspace/Chef-apache/.chef/agiletrailblazers.pem -N Rigil_Nod$
 
 # ssh into the chef node and execute the chef client to run its run list from chef server
-run-list(){
-ssh -i /home/ubuntu/.ssh/agiletrailblazers.pem ubuntu@54.175.232.159 "sudo chef-client";
-}
-echo "Run-list :$(run-list)"
+
+ssh -i /home/ubuntu/.ssh/agiletrailblazers.pem ubuntu@54.175.232.159 "sudo chef-client"
 
