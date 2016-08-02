127aa5c2aabb8fb4d8c36361bc77874f685f7d46
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-02 16:22:37 -0400

    Adding tomcat7 restart command

--- cookbooks/apache/recipes/tomcat7.rb
+++ cookbooks/apache/recipes/tomcat7.rb
@@ -3,12 +3,11 @@ execute "tomcat7 installation" do
  command "sudo apt-get install tomcat7"
 end
 
-template "/home/ubuntu/config.conf" do
-  source "template.erb"
-  variables( :a => 'Hello', :b => 'World', :c => 'Ololo' )
-end
-
 template "/etc/tomcat7/tomcat-users.xml" do
   source "tomcat-users.erb"
   variables( :user_name => 'admin', :creds => 'rigiladmin')
+end
+
+execute "tomcat7 restart" do
+ command "sudo service tomcat7 restart"
 end
