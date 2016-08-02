663292a9212627d44468e679a9bbc3108f996cf2
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-02 15:43:52 -0400

    edited knife.rb to key file paths

--- knife.rb
+++ knife.rb
@@ -2,10 +2,10 @@
 log_level                :info
 log_location             STDOUT
 node_name                'sasi'
-client_key               '/home/jenkins/workspace/Chef-apache/.chef/sasi.pem'
+client_key               '/home/ubuntu/.ssh/sasi.pem'
 validation_client_name   'sasi'
-validation_key           '/home/jenkins/workspace/Chef-apache/.chef/atb-chef-admin.pem'
+validation_key           '/home/ubuntu/.ssh/atb-chef-admin.pem'
 chef_server_url          'https://54.211.154.197/organizations/atb'
 #syntax_check_cache_path  '/home/jenkins/workspace/Chef-CookbookUpload/.chef/syntax_check_cache'
-cookbook_path            '/Users/sasikumaranandan/.chef/cookbooks/'
+cookbook_path            '/home/jenkins/workspace/Chef-apache/cookbooks/'
 #cookbook_path            ["#{current_dir}/../cookbooks/"]
