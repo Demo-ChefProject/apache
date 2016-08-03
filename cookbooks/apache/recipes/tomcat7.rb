71113abbbbf905680a8df9c155836a73d0b1a0eb
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-03 15:14:18 -0400

    editing files

--- cookbooks/apache/recipes/tomcat7.rb
+++ cookbooks/apache/recipes/tomcat7.rb
@@ -9,13 +9,23 @@ template "/etc/tomcat7/tomcat-users.xml" do
   variables( :user_name => 'admin', :creds => 'rigiladmin')
 end
 
-execute "deploy webapp to tomcat7" do
-  command "rm /var/lib/tomcat7/webapps/rigil-18f-pool2.war"
-  command "rm -rf /var/lib/tomcat7/webapps/rigil-18f-pool2"
-  
-  command "sudo wget http://54.175.158.124:8081/repository/Rigil/rigil-18f-pool2.war"
-  command "sudo mv rigil-18f-pool2.war /var/lib/tomcat7/webapps"
+
+remote_file '/home/ubuntu/rigil-18f-pool2.war' do
+  source 'http://54.175.158.124:8081/repository/Rigil/rigil-18f-pool2.war'
+  mode '0755'
+  action :create
 end
+
+
+#execute "deploy webapp to tomcat7" do
+#  command "rm /var/lib/tomcat7/webapps/rigil-18f-pool2.war"
+#  command "rm -rf /var/lib/tomcat7/webapps/rigil-18f-pool2"
+#  command "sudo wget http://54.175.158.124:8081/repository/Rigil/rigil-18f-pool2.war"
+
+#  command "sudo mv rigil-18f-pool2.war /var/lib/tomcat7/webapps"
+#end
+
+
 
 execute "tomcat7 restart" do
  command "sudo service tomcat7 restart"
