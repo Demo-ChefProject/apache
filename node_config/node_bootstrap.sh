0ec0eb195848c12e76ef5e33b7e42024190576a4
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-03 15:37:36 -0400

    editing files

--- node_config/node_bootstrap.sh
+++ node_config/node_bootstrap.sh
@@ -11,6 +11,8 @@ knife upload cookbooks apache
 # Bootstrap a node to its chef server
 sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/ubuntu/.ssh/agiletrailblazers.pem -N Rigil_Node -r 'role[apache]'
 
-# ssh into the chef node and execute the chef client to run its run list from chef server
+# Run this command to add the Host to the Jenkins slave(One time process)
+#ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/agiletrailblazers.pem ubuntu@54.175.232.159 "sudo chef-client"
 
+# ssh into the chef node and execute the chef client to run its run list from chef server
 ssh -i /home/ubuntu/.ssh/agiletrailblazers.pem ubuntu@54.175.232.159 "sudo chef-client"
