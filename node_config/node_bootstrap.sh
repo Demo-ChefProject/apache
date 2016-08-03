79e928725b9c3062cefdd2da22bfad0f65c18ff1
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-03 15:06:27 -0400

    editing files

--- node_config/node_bootstrap.sh
+++ node_config/node_bootstrap.sh
@@ -6,12 +6,11 @@ cd ~/home/jenkins/workspace/Chef-apache/
 sudo knife role from file /home/jenkins/workspace/Chef-apache/roles/apache.rb
 
 # Upload cookbooks into Chef Server
-knife upload cookbooks apache 
+knife upload cookbooks apache
 
 # Bootstrap a node to its chef server
 sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/ubuntu/.ssh/agiletrailblazers.pem -N Rigil_Node -r 'role[apache]'
 
 # ssh into the chef node and execute the chef client to run its run list from chef server
 
-ssh -i /home/ubuntu/.ssh/agiletrailblazers.pem ubuntu@54.175.232.159 "sudo chef-client"
-
+ssh -i /home/ubuntu/.ssh/agiletrailblazers.pem ubuntu@54.175.232.159 "sudo chef-client -l debug"
