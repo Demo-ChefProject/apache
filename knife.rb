796d7f8cda006b66a227346a7cb66d920639fe9f
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-03 14:52:16 -0400

    added log folder

--- knife.rb
+++ knife.rb
@@ -8,4 +8,6 @@ validation_key           '/home/ubuntu/.ssh/atb-chef-admin.pem'
 chef_server_url          'https://54.211.154.197/organizations/atb'
 #syntax_check_cache_path  '/home/jenkins/workspace/Chef-CookbookUpload/.chef/syntax_check_cache'
 cookbook_path            '/home/jenkins/workspace/Chef-apache/cookbooks/'
+knife[:node_log_max_files] = 9
+knife[:node_log_path] = '/var/log/chef-server/nodes'
 #cookbook_path            ["#{current_dir}/../cookbooks/"]
