56946b52fa4ac5a36f58802c13a3a93003e5f7e3
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-03 15:16:10 -0400

    editing files

--- cookbooks/apache/recipes/tomcat7.rb
+++ cookbooks/apache/recipes/tomcat7.rb
@@ -17,13 +17,12 @@ remote_file '/home/ubuntu/rigil-18f-pool2.war' do
 end
 
 
-#execute "deploy webapp to tomcat7" do
-#  command "rm /var/lib/tomcat7/webapps/rigil-18f-pool2.war"
-#  command "rm -rf /var/lib/tomcat7/webapps/rigil-18f-pool2"
+execute "deploy webapp to tomcat7" do
+  command "rm /var/lib/tomcat7/webapps/rigil-18f-pool2.war"
+  command "rm -rf /var/lib/tomcat7/webapps/rigil-18f-pool2"
 #  command "sudo wget http://54.175.158.124:8081/repository/Rigil/rigil-18f-pool2.war"
-
-#  command "sudo mv rigil-18f-pool2.war /var/lib/tomcat7/webapps"
-#end
+  command "sudo mv rigil-18f-pool2.war /var/lib/tomcat7/webapps"
+end
 
 
 
