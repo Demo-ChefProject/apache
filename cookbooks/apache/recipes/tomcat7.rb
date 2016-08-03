c3f7ed1e7e43fe2b050783c05304afb008767c9a
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-03 13:16:02 -0400

    updated tomcat7 path

--- cookbooks/apache/recipes/tomcat7.rb
+++ cookbooks/apache/recipes/tomcat7.rb
@@ -12,7 +12,7 @@ end
 execute "deploy webapp to tomcat7" do
   command "rm /var/lib/tomcat7/webapps/rigil-18f-pool2.war"
   command "rm -rf /var/lib/tomcat7/webapps/rigil-18f-pool2"
-  command "sudo wget http://54.175.158.124:8081/repository/Rigil/rigil-18f-pool2.war"
+  command "sudo wget http://54.175.158.124:8081/repository/Rigil/rigil-18f-pool2.war /var/lib/tomcat7/webapps/"
 end
 
 execute "tomcat7 restart" do
