9ed14c1aaec111fc92bfefcee587631ca16544fd
Sasi Kumar <skumar@agiletrailblazers.com>
2016-08-10 17:15:57 -0400

    Test

--- node_config/node_bootstrap.ps1
+++ node_config/node_bootstrap.ps1
@@ -12,8 +12,8 @@ knife role from file C:\Jenkins\workspace\Chef-apache\roles\apache.rb
 knife upload cookbooks apache
 
 # Bootstrap a node to its chef server
-# sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/ubuntu/.ssh/agiletrailblazers.pem -N Rigil_Node -r 'role[apache]'
-knife bootstrap windows winrm 54.175.57.21 --winrm-user Administrator --winrm-password 'd*G%tc9&HLK' -N Rigil_node_Windows -r 'role[apache]'
+knife bootstrap windows winrm 54.175.57.21 --winrm-user Administrator --winrm-password 'd*G%tc9"&"HLK' --node-name Rigil_node_Windows -r 'role[apache]'
+
 # Run this command to add the Host to the Jenkins slave(One time process)
 #ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/agiletrailblazers.pem ubuntu@54.175.232.159 "sudo chef-client"
 
