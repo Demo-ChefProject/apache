421afb72f91497f1f5f444108f77bed949d16622
Sasi Kumar <Sasi.Kumar@nc4.local>
2016-08-15 15:37:34 -0400

    added apache recipe

--- cookbooks/apache/recipes/apache.rb
+++ cookbooks/apache/recipes/apache.rb
@@ -1,4 +1,28 @@
 remote_file 'D:\NC4\MC3' do
-  source '
-  http://devmcnexus21.nc4.local:8081/repository/maven-releases/org/apache/apache-httpd-32/2.2.32/apache-httpd-32-2.2.32.zip'
+  source 'http://devmcnexus21.nc4.local:8081/repository/maven-releases/org/apache/apache-httpd-32/2.2.32/apache-httpd-32-2.2.32.zip'
+end
+
+execute "unzip apache" do
+  command "cd D:\NC4\MC3"
+  command "unzip apache-httpd-32-2.2.32.zip D:\NC4\MC3\HTTPD"
+end
+
+execute "remove logs" do
+  command "cd D:\NC4\MC3\HTTPD"
+  command "RD /S /Q 'D:\NC4\MC3\HTTPD\logs'"
+  command "RD /S /Q 'D:\NC4\MC3\HTTPD\errors'"
+end
+
+file "D:\NC4\MC3\HTTPD\conf\extra\MC3AgileDev.conf" do
+  source "httpd-vhosts.conf"
+end
+
+template "D:\NC4\MC3\HTTPD\conf\httpd.conf" do
+  source "httpd.erb"
+  variables( :server_name => 'MC3AgileDev')
+end
+
+execute "Create Windows server" do
+  command "cd D:\NC4\HTTPD\bin\"
+  command "httpd.exe -k install -n 'Apache 2.2 HTTP'"
 end
