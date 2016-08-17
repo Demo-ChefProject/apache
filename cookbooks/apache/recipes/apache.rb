f2c4c1a1019857acb62b1da5e7e132ca877ad888
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-17 14:41:08 -0400

    Update apache.rb

--- cookbooks/apache/recipes/apache.rb
+++ cookbooks/apache/recipes/apache.rb
@@ -23,7 +23,7 @@ template 'D:\NC4\MC3\HTTPD\conf\httpd.conf' do
 end
 
 execute 'Create Windows service for Apache' do
-  command 'cd D:\NC4\HTTPD\bin\'
-  command 'httpd.exe -k install -n 'Apache 2.2 HTTP''
+  command 'cd D:\NC4\HTTPD\bin'
+  command 'httpd.exe -k install -n "Apache 2.2 HTTP"'
   command 'sc \\server config ServiceName obj= Domain\user password= pass'
 end
