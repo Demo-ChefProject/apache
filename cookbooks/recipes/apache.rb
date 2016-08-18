9758be86b736c7547ce558269dcbc56f027b7d4b
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-18 15:55:45 -0400

    variable changes

--- cookbooks/recipes/apache.rb
+++ cookbooks/recipes/apache.rb
@@ -1,5 +1,6 @@
 remote_file 'D:\NC4\MC3' do
-  source 'http://devmcnexus21.nc4.local:8081/repository/maven-releases/org/apache/apache-httpd-32/2.2.32/apache-httpd-32-2.2.32.zip'
+  source 'vardeclarations.rb'
+  variables( :nexus => 'http://devmcnexus21.nc4.local:8081/repository/maven-releases/org/apache/apache-httpd-32/2.2.32/apache-httpd-32-2.2.32.zip')
 end
 
 execute 'unzip apache' do
